767f6461c6f8e2bbe234e25c0a9dd174b85aa49a
minseok980601 <minseok980601@gmail.com>
2023-03-26 01:00:48 +0900

    20230326 study

--- src/main/java/kr/board/controller/BoardController.java
+++ src/main/java/kr/board/controller/BoardController.java
@@ -3,33 +3,27 @@ package kr.board.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.board.entity.Board;
+import kr.board.mapper.BoardMapper;
 
 @Controller
 public class BoardController {
 
 	// /boardList.do
+	
+	@Autowired
+	private BoardMapper mapper;
+	
 	// HandlerMapping
 	@RequestMapping("/boardList.do")
 	public String boardList(Model model) {
 		
-		Board vo = new Board();
-		vo.setIdx(1);
-		vo.setTitle("게시판 실습");
-		vo.setContent("게시판 실습");
-		vo.setWriter("김민석");
-		vo.setIndate("2023-03-22");
-		vo.setCount(1);
-		
-		List<Board> list = new ArrayList<Board>();
-		list.add(vo);
-		list.add(vo);
-		list.add(vo);
-		
+		List<Board> list = mapper.getLists();
 		model.addAttribute("list", list);
 		
 		return "boardList";		// /WEB-INF/views/boardList.jsp -> forward
